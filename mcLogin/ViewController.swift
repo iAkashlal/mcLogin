ce3426a8c92ff70efdae27abbb44055e92b2c95f
Akashlal Bathe <tronicxo@gmail.com>
2019-11-03 23:17:36 +0530

    Changed directory structure

---
+++ mcLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mcLogin
+//
+//  Created by Akashlal on 10/11/19.
+//  Copyright © 2019 Akashlal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
