b783870b9d060ede9d1a9e69915b2588913ac7ad
Akashlal Bathe <tronicxo@gmail.com>
2019-11-10 13:17:02 +0530

    Connected and loaded web view screen

--- mcLogin/ViewControllers/DashboardViewController.swift
+++ mcLogin/ViewControllers/DashboardViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import WebKit
 
 class DashboardViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    var urlStr:String = "https://google.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: urlStr)!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
         // Do any additional setup after loading the view.
     }
 
