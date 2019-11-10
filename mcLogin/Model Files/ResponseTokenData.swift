baa42c9ae7b552651f97259e71fde27eeafa46a9
Akashlal Bathe <tronicxo@gmail.com>
2019-11-10 16:06:02 +0530

    Resuming building, added model file to map response

---
+++ mcLogin/Model Files/ResponseTokenData.swift
@@ -0,0 +1,10 @@
+//    ResponseTokenData.swift
+//    Model file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport
+
+import Foundation
+
+class ResponseTokenData : NSObject, Codable {
+
+    let token : String?
+
+}
